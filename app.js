84efce8a6d2bdd5528fbe09c016e38b245d8d7de
abideen-olawuwo <abideenolawuwo@gmail.com>
2023-06-08 21:24:50 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
