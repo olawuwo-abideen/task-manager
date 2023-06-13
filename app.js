7509ba664f1554b82b8f8a9209b972b705cf7882
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-06-13 13:21:52 +0100

    tasks

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-
-
+const tasks = require('./routes/tasks')
 const app = express();
 
 
+app.use(express.json())
 
 
+app.get('/hello', (req, res) => {
+    res.send('Welcome to My Tasks Manager')
+})
 
 
+app.use('/api/v1/tasks', tasks)
 
 
 
