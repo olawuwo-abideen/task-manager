ac7a4026b63eb3857d252c7a01514046070d937c
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-06-14 19:44:24 +0100

    error

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+
+
+
 const app = express();
-
-
 app.use(express.json())
 
-
+ 
 app.get('/hello', (req, res) => {
-    res.send('Welcome to My Tasks Manager')
+    res.send('Welcome to My Tasks Manager Api')
 })
 
-  
+app.use(notFound)
+app.use(errorHandlerMiddleware)   
 app.use('/api/v1/tasks', tasks)
 
 
