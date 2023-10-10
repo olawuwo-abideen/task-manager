547f30c6906a093e832b38428447c077127e9dd5
olawuwo-abideen <abideenolawuwo2000@gmail.com>
2023-10-10 16:00:27 +0100

    use cookie

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 const notFound = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
-
+var cookieParser = require('cookie-parser')
 
 
 const app = express();
@@ -15,6 +15,7 @@ app.get('/', (req, res) => {
     res.send('Welcome to My Tasks Manager Api')
 })
 
+app.use(cookieParser())
 app.use(notFound)
 app.use(errorHandlerMiddleware)   
 app.use('/api/v1/tasks', tasks)
